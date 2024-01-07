d3dbec24919b5e8afdd00c3e7433ad226e11fb12
HAMIMTHOYYIB <hamimthoyyib@gmail.com>
2024-01-07 22:01:51 +0530

    random updates

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on : http://localhost:${PORT}`);
 });
